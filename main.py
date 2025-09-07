3b354d694d03dfd5e28e6dc496d4c6ecc371c51b
flogit2161 <floj.jourdain@gmail.com>
2025-09-07 11:00:47 +0200

    group sprite creation

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
-        player.update(dt)
         dt = clock.tick(60) / 1000
         
     print("Starting Asteroids!")
